0835bda07d3c57edbc085f9a5d8e4aec98f30f4e
Matias Luhmann <139171817+Lumansito@users.noreply.github.com>
2024-08-10 17:52:40 -0300

    Creacion funcion asociacion

--- algoritmoGenetico.py
+++ algoritmoGenetico.py
@@ -3,11 +3,12 @@
 import time
 import random
 import os
+import math
 
 global numGenes,numIndividuos, individuos, valObj, porcFitness,probCrossover, probMutacion, valorMayorGlobal, valorMenorGlobal, menorGlobal, mayorGlobal, corridas, promedioValObjPorCorrida, nombreArchivoExcel
 numGenes = 4 #CANT DE CATEGORIAS
-numTuits= 5 #CANT DE TUITS POR INDIVIDUO
-numIndividuos = 5 #MODIFICAR LUEGO
+numTuits= 10 #CANT DE TUITS POR INDIVIDUO
+numIndividuos = 6 #MODIFICAR LUEGO
 probCrossover = 0.75
 probMutacion = 0.05
 valorMenorGlobal = 99999 ################ PARA GUARDAR DATOS POR CORRIDA, NO LOS USAMOS AUN
@@ -18,29 +19,69 @@ corridas = 0 # DIRIA QUE TRABAJEMOS CON CORRIDAS HASTA QUE EL USUARIO LAS CORTE,
 
 #Definimos el arreglo de individuos
 individuos = [[0 for i in range(numGenes)] for j in range(numIndividuos)] 
+tuitsAsociados = [[0 for i in range(numTuits)] for j in range(numIndividuos)] #para guardar los tuits asociados a cada individuo
 valObj = [0 for i in range(numIndividuos)]  #para guardar el valor de la func obj de cada individuo 
 porcFitness = [0 for i in range(numIndividuos)] #Para guaradar los valores del porcentaje que brinda la fun fitness para cada indiviuo
 
-#Incializar los valores que dependen de las corridas
-def inicializarValoresCorridas():
-    global minimoFo, maximoFo, cromosomasMaximos, cromosomasMinimos, promedioValObjPorCorrida, corridas, acumCorridas
-    
-    acumCorridas = 0
-    minimoFo = [0 for _ in range(corridas)]
-    maximoFo = [0 for _ in range(corridas)]
-    cromosomasMaximos = [0 for _ in range(corridas)]
-    cromosomasMinimos = [0 for _ in range(corridas)]
-    promedioValObjPorCorrida = [0 for _ in range(corridas)]
+
 
 def inicializarPoblacion(): #generamos numIndividuos individuos, con numTuits tuits distribuidos entre numGenes categorias
     global individuos
-    aux=0 #cant tuits
+    
     for i in range(numIndividuos):
         for j in range(numGenes):
-           while(aux<numTuits):
-            aux2= random.randint(0,numTuits-aux)
-            individuos[i][j] = aux2
-            aux+=aux2
+            if i==j:
+                individuos[i][j] = 5  #ponderar una de las categorias en cada tuit distinto
+            elif i<numGenes:
+                individuos[i][j] = random.randint(0,3)  #como va a estar con un tuit ponderado, el resto de los tuits se generan con valores menores
+            else:
+                individuos[i][j] = random.randint(0,5)
+
+
+    print("Poblacion inicial: ", individuos)
+
+
+from tuits_categorizados import tuit_categoria 
+
+def asociarTuits(): #asocia los tuits a cada individuo
+    global tuitsAsociados
+    for i in range(numIndividuos):
+        partes = sum(individuos[i])  #[0, 1, 3, 1] -> 5 ['animal'] * 60 + ['economía'] * 60 + ['tecnología'] * 60 + ['deportes']
+        print("##math##")
+        animal = math.ceil((individuos[i][0]/partes)*numTuits)
+        economia = math.ceil((individuos[i][1]/partes)*numTuits)   #math.ceil para redondear hacia arriba
+        tecnologia = math.ceil((individuos[i][2]/partes)*numTuits)
+        deportes = math.ceil((individuos[i][3]/partes)*numTuits)
+
+        while(animal+economia+tecnologia+deportes>numTuits):    #Me aseguro que la suma sea = a numTuits
+            maximo = max(animal, economia, tecnologia, deportes) 
+            if maximo == animal:
+                animal-=1
+            elif maximo == economia:    
+                economia-=1
+            elif maximo == tecnologia:
+                tecnologia-=1
+            else:
+                deportes-=1
+
+        for j in range(numTuits):
+            if j<animal:
+                tuitsAsociados[i][j] = tuit_categoria[0][random.randint(0,9)] #se asocia un tuit de la categoria animal(la posicion 0)
+            elif j<animal+economia:
+                tuitsAsociados[i][j] = tuit_categoria[1][random.randint(0,9)]
+            elif j<animal+economia+tecnologia:
+                tuitsAsociados[i][j] = tuit_categoria[2][random.randint(0,9)]
+            else:
+                tuitsAsociados[i][j] = tuit_categoria[3][random.randint(0,9)]
+    
+        
+    print("Tuits asociados: ", tuitsAsociados)
+
+        
+        
+            
+            
+
 
 op= ""
 reaccion = ""
@@ -59,9 +100,10 @@ todas las corridas que hagamos sean iguales)
 Se debería generar un fitness del tamaño numIndividuos que almacene el fitness de cada ind al final de cada uno
 Luego se hacen selec, cross y mut y se repite
 ''' 
-inicializarValoresCorridas() #no se si nos va a servir esto
-inicializarPoblacion() #se genera la poblacion inicial de individuos
 
+inicializarPoblacion() #se genera la poblacion inicial de individuos
+asociarTuits() #se asocian los tuits a cada individuo
+'''
 while op.upper() =="C": #si el usuario desea comenzar el experimento
     for i in range (numIndividuos): #para cada individuo de la poblacion
         likes=0
@@ -97,3 +139,5 @@ while op.upper() =="C": #si el usuario desea comenzar el experimento
     #se genera un nuevo array de individuos y se reinicia el while en una nueva corrida
 #mostrar datos del experimento en general
 print("\n--- Fin del programa ---")
+
+'''
---
+++ tuits_categorizados.py
@@ -0,0 +1,69 @@
+# Lista de tuplas con 10 tuits por cada categoría
+tuit_categoria = [
+
+    # Animal
+    [
+        "Los elefantes son conocidos por su memoria",
+        "El tigre es un felino en peligro de extinción",
+        "Las aves migratorias recorren largas distancias",
+        "El perro es considerado el mejor amigo del hombre",
+        "Los gatos son animales muy independientes",
+        "El león es conocido como el rey de la selva",
+        "Los delfines son animales muy inteligentes",
+        "Las abejas juegan un papel crucial en la polinización",
+        "El canguro es un símbolo de Australia",
+        "Las ballenas son los mamíferos más grandes del mundo"
+    ],
+    # Economía
+    [
+        "La inflación afecta a todos los sectores económicos",
+        "El mercado bursátil cerró con pérdidas hoy",
+        "El dólar sigue subiendo en los mercados internacionales",
+        "El gobierno anuncia nuevas reformas fiscales",
+        "El desempleo ha disminuido este trimestre",
+        "La inversión extranjera en el país ha crecido",
+        "Las exportaciones han aumentado en el último año",
+        "La economía global enfrenta una recesión",
+        "El precio del petróleo ha caído significativamente",
+        "Las criptomonedas han ganado popularidad"
+    ],
+
+    
+    
+    #tecnologia
+    [
+        "El nuevo iPhone ha sido presentado al público",
+        "La inteligencia artificial está revolucionando la tecnología",
+        "El software de código abierto es una tendencia creciente",
+        "La realidad virtual ofrece nuevas experiencias inmersivas",
+        "El internet de las cosas conecta dispositivos en red",
+        "Los videojuegos son una forma popular de entretenimiento",
+        "La robótica avanza a pasos agigantados",
+        "La ciberseguridad es una preocupación creciente",
+        "El streaming de música ha cambiado la industria",
+        "Los teléfonos inteligentes son cada vez más potentes"
+    ],
+    # Deportes  
+    [
+        "El equipo ganó el partido en los últimos minutos",
+        "Los Juegos Olímpicos comenzarán el próximo mes",
+        "El fútbol es el deporte más popular del mundo",
+        "El jugador estableció un nuevo récord",
+        "El campeonato de tenis está en su fase final",
+        "La liga de baloncesto ha sido muy competitiva este año",
+        "El maratón atrajo a corredores de todo el mundo",
+        "El ciclismo de montaña es un deporte emocionante",
+        "El equipo nacional se clasificó para el Mundial",
+        "La natación es una excelente forma de ejercicio"
+    ],
+    
+]
+
+# Ejemplo de acceso al segundo tuit en la lista
+tweet_ejemplo = tuit_categoria[1]
+
+
+print(tweet_ejemplo) # Salida: ('Los Juegos Olímpicos comenzarán el próximo mes', 'deportes')
+
+#twwet_ejemplo[0] Salida: 'Los Juegos Olímpicos comenzarán el próximo mes'
+#tweet_ejemplo[1] Salida: 'deportes'
