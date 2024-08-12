17dead98940effe3419e6c1d433bb03969673bed
Matias Luhmann <139171817+Lumansito@users.noreply.github.com>
2024-08-12 10:13:43 -0300

    update

--- algoritmoGenetico.py
+++ algoritmoGenetico.py
@@ -7,7 +7,7 @@ import math
 
 global numGenes,numIndividuos, individuos, valObj, porcFitness,probCrossover, probMutacion, valorMayorGlobal, valorMenorGlobal, menorGlobal, mayorGlobal, corridas, promedioValObjPorCorrida, nombreArchivoExcel
 numGenes = 4 #CANT DE CATEGORIAS
-numTuits= 10 #CANT DE TUITS POR INDIVIDUO
+numTuits= 6 #CANT DE TUITS POR INDIVIDUO
 numIndividuos = 6 #MODIFICAR LUEGO
 probCrossover = 0.75
 probMutacion = 0.05
@@ -22,18 +22,18 @@ individuos = [[0 for i in range(numGenes)] for j in range(numIndividuos)]
 tuitsAsociados = [[0 for i in range(numTuits)] for j in range(numIndividuos)] #para guardar los tuits asociados a cada individuo
 valObj = [0 for i in range(numIndividuos)]  #para guardar el valor de la func obj de cada individuo 
 porcFitness = [0 for i in range(numIndividuos)] #Para guaradar los valores del porcentaje que brinda la fun fitness para cada indiviuo
-
-
+cantidadTuits =[[0 for i in range(numGenes)] for j in range(numIndividuos)]
+cantLikes = [0 for i in range(numIndividuos)] #almacena la cantidad de likes de cada individuo, cada iteracion
 
 def inicializarPoblacion(): #generamos numIndividuos individuos, con numTuits tuits distribuidos entre numGenes categorias
     global individuos
-    
+                                    #REVISAR LA PONDERACION AL AGREGAR MAS CATEGORIAS
     for i in range(numIndividuos):
-        for j in range(numGenes):
+        for j in range(numGenes):   #[1,1,1,1][2,2,2,2][3,3,3,3][4,4,4,4]   [][]
             if i==j:
                 individuos[i][j] = 5  #ponderar una de las categorias en cada tuit distinto
             elif i<numGenes:
-                individuos[i][j] = random.randint(0,3)  #como va a estar con un tuit ponderado, el resto de los tuits se generan con valores menores
+                individuos[i][j] = random.randint(0,2)  #como va a estar con un tuit ponderado, el resto de los tuits se generan con valores menores
             else:
                 individuos[i][j] = random.randint(0,5)
 
@@ -44,45 +44,44 @@ def inicializarPoblacion(): #generamos numIndividuos individuos, con numTuits tu
 from tuits_categorizados import tuit_categoria 
 
 def asociarTuits(): #asocia los tuits a cada individuo
-    global tuitsAsociados
+    global tuitsAsociados, cantidadTuits
     for i in range(numIndividuos):
-        partes = sum(individuos[i])  #[0, 1, 3, 1] -> 5 ['animal'] * 60 + ['economía'] * 60 + ['tecnología'] * 60 + ['deportes']
+        partes = sum(individuos[i])  #[0, 1, 3, 1] -> 5 
         print("##math##")
-        animal = math.ceil((individuos[i][0]/partes)*numTuits)
-        economia = math.ceil((individuos[i][1]/partes)*numTuits)   #math.ceil para redondear hacia arriba
-        tecnologia = math.ceil((individuos[i][2]/partes)*numTuits)
-        deportes = math.ceil((individuos[i][3]/partes)*numTuits)
-
-        while(animal+economia+tecnologia+deportes>numTuits):    #Me aseguro que la suma sea = a numTuits
-            maximo = max(animal, economia, tecnologia, deportes) 
-            if maximo == animal:
-                animal-=1
-            elif maximo == economia:    
-                economia-=1
-            elif maximo == tecnologia:
-                tecnologia-=1
-            else:
-                deportes-=1
-
-        for j in range(numTuits):
-            if j<animal:
-                tuitsAsociados[i][j] = tuit_categoria[0][random.randint(0,9)] #se asocia un tuit de la categoria animal(la posicion 0)
-            elif j<animal+economia:
-                tuitsAsociados[i][j] = tuit_categoria[1][random.randint(0,9)]
-            elif j<animal+economia+tecnologia:
-                tuitsAsociados[i][j] = tuit_categoria[2][random.randint(0,9)]
-            else:
-                tuitsAsociados[i][j] = tuit_categoria[3][random.randint(0,9)]
-    
-    for i in range(numIndividuos):
-        print("Individuo ", individuos[i], "Tuits asociados: ", tuitsAsociados[i])    
-        print("\n")
-    
-
+        for j in range(numGenes):
+            cantidadTuits[i][j] = math.ceil((individuos[i][j]/partes)*numTuits)
         
+        while(sum(cantidadTuits[i])>numTuits):    #Me aseguro que la suma sea = a numTuits ver si hacer random
+            posicion = random.randint(0,numGenes-1) 
+            if cantidadTuits[i][posicion]>0:
+                cantidadTuits[i][posicion]-=1
         
-            
-            
+      
+        posTuitAsoc=0
+        for j in range(numGenes): #parados en el individuo i recorremos cada una de las 4 categorias j
+            for k in range (cantidadTuits[i][j]): #la cantidad de tuits que tenga para esa categoria
+                tuitsAsociados[i][posTuitAsoc] = tuit_categoria[j][random.randint(0,9)] #le asocio k tuits de la categoria j
+                posTuitAsoc+=1 #para que no sobreescriba sobre los mismo tuits
+
+    
+def ruleta():
+    global individuos
+    hijos = [[0 for i in range(numGenes)] for j in range(numIndividuos)] #para guardar los hijos
+    porcentajes = [0 for i in range(1000)]  #inicializamos el arreglo de la ruleta
+    indAct = 0 #para llevar la cuenta de las posiciones que se van llenando
+    
+    for i in range(numIndividuos): 
+        fit = int(porcFitness[i]*1000)
+        for j in range(fit):
+            porcentajes[indAct+j] = i
+        indAct = indAct + fit
+    for i in range(numIndividuos): #numIndividuos veces se hace la ruleta para llenar el resto
+        if(indAct == 1000):   #para evitar que el arreglo porcetajes se exceda del limite
+            indAct = 999
+        num = random.randint(0,indAct) #para no usar 1000 y agregarle posibilidades al ind 0 debido al truncamiento
+        aux = porcentajes[num]
+        hijos [i] = individuos[aux].copy() #paso por valor a un array hijos los ganadores
+    individuos = hijos
 
 
 op= ""
@@ -105,25 +104,25 @@ Luego se hacen selec, cross y mut y se repite
 
 inicializarPoblacion() #se genera la poblacion inicial de individuos
 asociarTuits() #se asocian los tuits a cada individuo
-'''
+
 while op.upper() =="C": #si el usuario desea comenzar el experimento
     for i in range (numIndividuos): #para cada individuo de la poblacion
-        likes=0
         for j in range (numTuits): #para cada tuit del individuo
             if (reaccion !="F"): #si el usuario no desea terminar el experimento
                 time.sleep(0.5)
                 os.system('cls')
                 time.sleep(0.5)
-                print("individuo: ", i, "tuit: ", j, "likes de este individuo: ", likes) #muestra datos del tuit (solo en desarrollo, despues sacar)
+                print("individuo: ", i, "tuit: ", j, "likes de este individuo: ", cantLikes[i]) #muestra datos del tuit (solo en desarrollo, despues sacar)
                 
-                print (" \033[92mtuit gracioso o interesante lalalala\033[0m") #muestra el tuit y pide que de Like, No Like o Finalizar
+                print ("\033[92m",tuitsAsociados[i][j],"\033[0m") #muestra el tuit y pide que de Like, No Like o Finalizar
                 
                 reaccion= input("   L - Like\n   N - No Like (continuar)\n   F - Finalizar experimento\nIngrese la opcion deseada: ").upper()
                 while reaccion != "L" and reaccion != "N" and reaccion != "F":
                     print("Opcion no valida")
                     reaccion= input("   L - Like\n   N - No Like (continuar)\n   F - Finalizar experimento\nIngrese la opcion deseada: ").upper()
+               
                 if (reaccion =="L"):
-                    likes+=1
+                    cantLikes[i]+=1
                 elif(reaccion =="F"): #pregunta para ver si no fue un typo, si quiere terminar, dejarlo, si no modificar reaccion
                     reaccion= input("esta seguro que desea terminar el experimento? F - finalizar /N - no finalizar").upper() #validar
             else: #si desea finalizar
@@ -139,7 +138,12 @@ while op.upper() =="C": #si el usuario desea comenzar el experimento
     #realizar seleccion, crossover y mutacion
     #guardar datos si es pertinente
     #se genera un nuevo array de individuos y se reinicia el while en una nueva corrida
+    #se reinicia el array de likes
 #mostrar datos del experimento en general
+
+print("Datos del experimento")
+print(individuos)
+print(cantidadTuits)
+print(cantLikes)
 print("\n--- Fin del programa ---")
 
-'''
